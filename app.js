a651d1ab86b49ee732442916940fd3bfb5498bc1
Blue Tortoise <dianmak@icloud.com>
2019-05-04 13:51:50 -0400

    added form under JET SET

--- app.js
+++ app.js
@@ -10,11 +10,28 @@ $(".dropdown-item").click(function () {
     }).then(function (response) {
         console.log(response);
 
-
         $(".robbery").html(
-            "<h3> Incident Count" + response.results[response.results.length - 1].incident_count + "</h3>"
-            + "<h3> Offense Count" + response.results[response.results.length - 1].offense_count + "</h3>" +
-            "<h3> Year" + response.results[response.results.length - 1].data_year + "</h3>");
+            "<h2> Robbery </h2> <h3> Incident Count " + response.results[response.results.length - 1].incident_count + "</h3>"
+            + "<h3> Offense Count " + response.results[response.results.length - 1].offense_count + "</h3>" +
+            "<h3> Year " + response.results[response.results.length - 1].data_year + "</h3>");
     })
+
+    $.ajax({
+        url: "https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/rape/offense/states/MA/count?api_key=CMQzjoaW3P43pwPkMnWzLcqwhlVQXZcJeo8rrktf",
+        method: "GET"
+    }).then(function (response) {
+
+        console.log(response);
+        // console.log(url);
+        // console.log(response);
+        console.log(response.results.length)
+
+        $(".rape").html(
+            "<h3>" + response.results[response.results.length - 1].incident_count + "</h3>"
+        );
+
+        $(".offense_count").text("<h3>" + response.results[response.results.length - 1].offense_count + "</h3>");
+        $(".data_year").text("<h3>" + response.results[response.results.length - 1].data_year + "</h3>");
+    });
 });
 
